d5990db36b7c54415457a8f74cedb32b5ea7e2b1
Parkwochang <whwh3560@gmail.com>
2023-08-20 13:15:09 +0900

    style error

---
+++ packages/nextjs/tailwind.config.cjs
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '**/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    '**/components/**/*.{js,ts,jsx,tsx,mdx}',
+    '**/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Pretendard', 'system-ui', 'sans-serif'],
+    },
+    fontWeight: {
+      thin: '100',
+      extralight: '200',
+      light: '300',
+      normal: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
+      extrabold: '800',
+      black: '900',
+    },
+    extend: {
+      fontSize: {
+        '2xs': '10px',
+      },
+      colors: {
+        bible: '#2AC1BC',
+        img: '#FFC8A2',
+        main: '#3182f6',
+      },
+    },
+  },
+  // plugins: [require('tailwind-scrollbar')],
+};
